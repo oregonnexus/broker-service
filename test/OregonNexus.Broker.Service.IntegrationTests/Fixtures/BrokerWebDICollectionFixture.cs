e6567a4a2767b1ad0e4acf34286790d0e02f177e
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-11-19 22:28:00 +0000

    Remove files that are now ignored

--- test/OregonNexus.Broker.Service.IntegrationTests/Fixtures/BrokerWebDICollectionFixture.cs
+++
@@ -1,6 +0,0 @@
-namespace OregonNexus.Broker.Service.IntegrationTests.Fixtures;
-
-[CollectionDefinition("BrokerWebDICollection")]
-public class BrokerWebDICollectionFixture : ICollectionFixture<BrokerWebDIServicesFixture>
-{
-}
